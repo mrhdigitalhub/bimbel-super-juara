6a04d273a7573d1b1e68350f82bbf8eeef368b2e
Rismawan Maulana Sidiq <rismawanm170@gmail.com>
2026-09-07 13:31:30 +0700

    fix: all pages dynamic - juara ready

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+export const dynamic = 'force-dynamic';
 import { useEffect, useState } from "react";
 import { createClient } from "@supabase/supabase-js";
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || "";
